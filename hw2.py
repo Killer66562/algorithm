a50ab9803f32acfb52d49796a0c6bcc26096e533
Killer66562 <jimmy688382@gmail.com>
2024-11-06 21:54:06 +0800

    Yes

--- hw2.py
+++ hw2.py
@@ -1,6 +1,7 @@
-from typing import Self, Callable
+from statistics import median
+from random import randint, random
+from typing import Callable
 from time import time
-from random import randint
 
 
 class Point(object):
@@ -10,55 +11,43 @@ class Point(object):
         self.rank = 0
 
     def __repr__(self) -> str:
-        return f"({self.x}, {self.y}) => Rank: {self.rank}"
-    
-    def __gt__(self, point: Self) -> bool:
-        return self.x > point.x and self.y > point.y
-    
-    def __eq__(self, point: Self) -> bool:
-        return self.x == point.x and self.y == point.y
-    
-    def __lt__(self, point: Self) -> bool:
-        return self.x < point.x and self.y < point.y
+        return "(%.2f %.2f) => rank: %d" % (self.x, self.y, self.rank)
 
-
-def rank_slow(arr: list[Point]):
+def mininum_2d_ranking_slow(arr: list[Point]):
     for p1 in arr:
         for p2 in arr:
-            if p1 > p2:
+            if p1.x > p2.x and p1.y > p2.y:
                 p1.rank += 1
 
-def rank_medium(arr: list[Point]):
-    arr.sort(key=lambda p: (p.x, p.y), reverse=True)
-    n = len(arr)
-
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            if arr[i] > arr[j]:
-                arr[i].rank += 1
-
-def rank_fast(arr: list[Point]):
+def mininum_2d_ranking_fast(arr: list[Point]) -> None:
     n = len(arr)
     if n <= 1:
         return
-    else:
-        arr.sort(key=lambda p: (p.x, p.y)) #O(nlog(n))
-        med = n // 2
+    
+    med = median([p.x for p in arr])
+    a = [p for p in arr if p.x <= med]
+    b = [p for p in arr if p.x > med]
 
-        rank_fast(arr[0:med])
-        rank_fast(arr[med:n])
+    if not a or not b:
+        a = [p for p in arr if p.x < med]
+        b = [p for p in arr if p.x >= med]
 
-        count = 0
+    if a and b:
+        a_len = len(a)
 
-        for p1 in arr[med:n]:
-            for p2 in arr[count:med]:
-                if p1 > p2:
-                    count += 1
-                else: #提早退出
-                    p1.rank += count
-                    break
-            else: #比後面的都來得大所以要加上count
-                p1.rank += count
+        mininum_2d_ranking_fast(a)
+        mininum_2d_ranking_fast(b)
+
+        a.sort(key=lambda p: p.y)
+        b.sort(key=lambda p: p.y)
+
+        a_idx = 0
+        for p in b:
+            while a_idx < a_len and a[a_idx].y < p.y:
+                a_idx += 1
+            p.rank += a_idx
+
+        arr = a + b
 
 def timing(func: Callable, *args) -> float:
     start_time = time()
@@ -66,28 +55,78 @@ def timing(func: Callable, *args) -> float:
     end_time = time()
     return end_time - start_time
 
-def test(test_times: int = 5, n = 1000, min_val = -5000, max_val = 5000) -> None:
-    for i in range(1, test_times + 1):
-        arr1 = [Point(x, y) for x, y in zip((randint(min_val, max_val) for _ in range(n)), (randint(min_val, max_val) for _ in range(n)))]
-        arr2 = [Point(p.x, p.y) for p in arr1]
+def sort_and_compare_arr(arr1: list[Point], arr2: list[Point]) -> bool:
+    arr1.sort(key=lambda p: (p.x, p.y), reverse=True)
+    arr2.sort(key=lambda p: (p.x, p.y), reverse=True)
 
-        time_fast = timing(rank_fast, arr1)
-        time_slow = timing(rank_slow, arr2)
+    arr1_len = len(arr1)
+    arr2_len = len(arr2)
 
-        print(f"Test #{i} (fast): {time_fast} secs")
-        print(f"Test #{i} (slow): {time_slow} secs")
-
-        arr1.sort(key=lambda p: (p.x, p.y), reverse=True)
-        arr2.sort(key=lambda p: (p.x, p.y), reverse=True)
-
-        if arr1 != arr2:
-            print("Test failed!")
-            break
+    if arr1_len != arr2_len:
+        return False
+    
+    for i in range(arr1_len):
+        p1 = arr1[i]
+        p2 = arr2[i]
+        if p1.x != p2.x or p1.y != p2.y or p1.rank != p2.rank:
+            return False
     else:
-        print("Test passed")
+        return True
+    
+def print_ranking_result(arr: list[Point]):
+    for p in arr:
+        print(p)
+
+    arr.sort(key=lambda p: p.rank)
+
+    min_rank_p = arr[0]
+    max_rank_p = arr[-1]
+
+    print(f"Max rank: {max_rank_p.rank}")
+    print(f"Min rank: {min_rank_p.rank}")
+    
+def create_test_data(n: int = 2000, min_val: int = -5000, max_val: int = 5000, filename: str = "test2.txt"):
+    if n < 1:
+        print("n >= 1 required")
+        return
+
+    arr = [Point(x, y) for x, y in zip((randint(min_val, max_val) * random() for _ in range(n)), (randint(min_val, max_val) * random() for _ in range(n)))]
+
+    with open(file=filename, mode='w', encoding="utf8") as file:
+        for p in arr:
+            text = "%.2f %.2f\r\n" % (p.x, p.y)
+            file.write(text)
+
+    mininum_2d_ranking_slow(arr)
+    print_ranking_result(arr)
+
+def load_test_data_and_solve(filename: str = "test2.txt"):
+    arr = []
+
+    with open(file=filename, mode='r', encoding="utf8") as file:
+        while True:
+            try:
+                x_y_str = file.readline()
+                #Check EOF while using readline
+                if not x_y_str:
+                    break
+                x_y_str = x_y_str.strip()
+                #After strip, an empty string may appear
+                if not x_y_str:
+                    continue
+                x_y_list = list(map(float, x_y_str.split()))
+                x = x_y_list[0]
+                y = x_y_list[1]
+                arr.append(Point(x, y))
+            except EOFError:
+                break
+
+    mininum_2d_ranking_fast(arr)
+    print_ranking_result(arr)
 
 def main():
-    test(n=5000, min_val=-10, max_val=10)
+    #create_test_data()
+    load_test_data_and_solve()
 
 if __name__ == "__main__":
     main()
